1c3f17a1c522abd113affe44e2b805c5a2ea5a98
Claudio9905 <cloudpost10@gmail.com>
2025-08-06 16:35:06 +0200

    Update File

---
+++ src/components/Favourites.jsx
@@ -0,0 +1,45 @@
+import { useSelector } from "react-redux";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Row, Col, Container } from "react-bootstrap";
+
+const Favourites = () => {
+  const jobs = useSelector((state) => {
+    return state.favourites.content;
+  });
+
+  return (
+    <>
+      <div className="mt-4 border border-3 border-danger-subtle rounded-3 w-25 text-center ms-3 bg-dark shadow-lg">
+        <h1 className="fs-1 text-light">Pagina Preferiti</h1>
+      </div>
+      {/* aggiungo l'azienda che ho selezionate per inserirlo nella lista dei preferiti */}
+      <Container>
+        <Row>
+          <Col xs="12">
+            <ListGroup>
+              {jobs.map((job, i) => {
+                <ListGroup.Item key={i} className="my-3">
+                  <div className="d-flex flex-column">
+                    <h2>{job.title}</h2>
+                    <h4>{job.company_name}</h4>
+                    <h4>{job.url}</h4>
+                  </div>
+                  <div className="d-flex flex-column">
+                    <p>
+                      {job.category}-{job.job_type}
+                    </p>
+                  </div>
+                  <div>
+                    <p>{job.description}</p>
+                  </div>
+                </ListGroup.Item>;
+              })}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Favourites;
